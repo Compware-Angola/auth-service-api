ea3bd3670fd2b9a5145ff58f29b83a1a4cac4823
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2025-12-01 20:01:42 +0100

    feat:update data

--- src/auth/dto/send-renew-data.dto.ts
+++ src/auth/dto/send-renew-data.dto.ts
@@ -8,6 +8,7 @@ import {
   Matches,
   IsEnum,
   IsOptional,
+  Min,
 } from 'class-validator';
 import { AuthPlatform } from './signIn.dto';
 
@@ -47,7 +48,7 @@ export class SendRenewDataDto {
   })
   @IsString()
   @IsNotEmpty()
-  @Length(20, 500)
+  @Min(10, { message: 'O motivo deve ter no mínimo 10 caracteres' })
   details: string;
 
   @ApiProperty({
